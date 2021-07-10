33f51604144d7e0ea3553b85e87988a4df5b859d
Matthew Carlson <matt@mcarlson.xyz>
2021-07-10 02:01:50 +0000

    clean up

--- config.h
+++ config.h
@@ -1,19 +1,36 @@
+// herbe
+
+// colors
 static const char *background_color = "#3e3e3e";
 static const char *border_color = "#ececec";
 static const char *font_color = "#ececec";
+
+// font
 static const char *font_pattern = "monospace:size=10";
+
+// line spacing
 static unsigned line_spacing = 5;
+
+// padding
 static unsigned int padding = 15;
 
+// width
 static unsigned int width = 450;
+
+// border size
 static unsigned int border_size = 2;
+
+// positions
 static unsigned int pos_x = 30;
 static unsigned int pos_y = 60;
 
+// corners
 enum corners { TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT };
 enum corners corner = TOP_RIGHT;
 
-static unsigned int duration = 5; /* in seconds */
+// time on screen
+static unsigned int duration = 5;
 
+// button defs
 #define DISMISS_BUTTON Button1
 #define ACTION_BUTTON Button3
